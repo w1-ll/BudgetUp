2d1beac9df6d6dfa8c10fb9aba41f2512b101d57
Rishitha Bura <rishithabura@gmail.com>
2024-03-09 21:07:40 -0500

    Homepage design completed.

--- budgetup/src/components/navBar.js
+++ budgetup/src/components/navBar.js
@@ -1,64 +1,47 @@
-import React from 'react';
+import React,{useState} from "react";
+import Sidebar from "./sideBar.js"
 
-function navBar(){
+
+function NavBar(){
+  const [sidebarVisible, setSidebarVisible] = useState(false);
+  const toggleSidebar = () => {
+    setSidebarVisible(!sidebarVisible);
+  };
+  const noShow=()=>{
+    setSidebarVisible(false)
+  }
     return (
         <>
-        <div className="flex flex-col bg-lightcolor">
-          <div className="flex z-10 flex-col pb-0 w-full max-md:max-w-full">
-
-            <div className="flex justify-center rounded-b-xl items-center py-1.0 w-full whitespace-nowrap bg-sky-600 max-md:px-5 max-md:max-w-full ">
-            <div className="text-white pb-1 text-2xl font-bold">Menu</div>
-              <div className="px-16">
-              <div className="flex justify-between gap-3 w-full max-w-[1097px] max-md:flex-wrap max-md:max-w-full">
-
-                <div className="flex justify-between gap-3 pb-1 items-center max-md:flex-wrap max-md:max-w-full">
-                  <div className="text-white pb-1 text-2xl font-bold">BudgetUp!</div>
-                </div>
-                
-               <div className="flex gap-5 justify-between my-auto text-xl text-black">
-
-                <div className="grow justify-center px-4 py-3.5 bg-[linear-gradient(0deg,#F4F3EE_0%,#F4F3EE_100%),#89023E] rounded-[50px]">
-                    <button
-                      type="button"
-                      className="bg-white text-gray-900 bg-lightcolor border border-gray-300 focus:outline-none 
-              hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 
-              dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 
-              dark:focus:ring-gray-700"
-                    >
-                      About
-                    </button>
-                  </div>
-
-                  <div className="grow justify-center px-4 py-3.5 bg-[linear-gradient(0deg,#F4F3EE_0%,#F4F3EE_100%),#89023E] rounded-[50px]">
-                    <button
-                      type="button"
-                      className="bg-white text-gray-900 bg-lightcolor border border-gray-300 focus:outline-none 
-              hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 
-              dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 
-              dark:focus:ring-gray-700"
-                    >
-                      Sign in
-                    </button>
-                  </div>
-                  <div className="grow justify-center px-3 py-3.5 bg-[linear-gradient(0deg,#F4F3EE_0%,#F4F3EE_100%),#89023E] rounded-[50px]">
-                    <button
-                      type="button"
-                      className="bg-white text-gray-900 bg-lightcolor border border-gray-300 focus:outline-none 
-              hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 
-              dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 
-              dark:focus:ring-gray-700"
-                    >
-                      Register
-                    </button>
-                  </div>
-                </div>
-              </div>
-              </div>
-            </div>
-          </div>
+        <div className="z-10">
+        {sidebarVisible && <Sidebar show={sidebarVisible} onClose={noShow}/>}
         </div>
         
+        <div className="flex flex-col font-bold bg-white">
+
+        <div className="flex justify-center items-center px-16 py-2.5 w-full whitespace-nowrap bg-lightBlue max-md:px-5 max-md:max-w-full">
+        <div className="flex gap-5 justify-between w-full max-w-[1175px] max-md:flex-wrap max-md:max-w-full">
+          <div className="flex gap-5 justify-between text-4xl text-white">
+            <button onClick={toggleSidebar} className="justify-center px-5 py-3 bg-lightBlue rounded-3xl border border-black border-solid shadow-sm">
+              ☰
+            </button>
+            <div className="flex-auto my-auto">BudgetUp</div>
+          </div>
+          
+          <div className="flex gap-5 justify-between items-start my-auto text-xl text-black max-md:flex-wrap">
+            <button className="grow justify-center px-9 py-2 bg-white rounded-3xl max-md:px-5">
+              About
+            </button>
+            <button className="grow justify-center px-8 py-2 bg-white rounded-3xl max-md:px-5">
+              Sign in
+            </button>
+            <button className="grow justify-center self-stretch px-7 py-2 bg-white rounded-3xl max-md:px-5">
+              Sign up
+            </button>
+          </div>
+        </div>
+      </div>
+      </div>
         </>
     );
 }
-export default navBar;
+export default NavBar;
--- budgetup/src/pages/Home.js
+++ budgetup/src/pages/Home.js
@@ -1,30 +1,13 @@
 import NavBar from '../components/navBar.js';
-import * as React from "react";
+import React,{useState} from "react";
 
 function Home() {
+  
   return (
+    <>
+    <NavBar />
     <div className="flex flex-col font-bold bg-white">
-      <div className="flex justify-center items-center px-16 py-2.5 w-full whitespace-nowrap bg-lightBlue max-md:px-5 max-md:max-w-full">
-        <div className="flex gap-5 justify-between w-full max-w-[1175px] max-md:flex-wrap max-md:max-w-full">
-          <div className="flex gap-5 justify-between text-4xl text-white">
-            <button className="justify-center px-5 py-3 bg-lightBlue rounded-3xl border border-black border-solid shadow-sm">
-              ☰
-            </button>
-            <div className="flex-auto my-auto">BudgetUp</div>
-          </div>
-          <div className="flex gap-5 justify-between items-start my-auto text-xl text-black max-md:flex-wrap">
-            <button className="grow justify-center px-9 py-2 bg-white rounded-3xl max-md:px-5">
-              About
-            </button>
-            <button className="grow justify-center px-8 py-2 bg-white rounded-3xl max-md:px-5">
-              Sign in
-            </button>
-            <button className="grow justify-center self-stretch px-7 py-2 bg-white rounded-3xl max-md:px-5">
-              Sign up
-            </button>
-          </div>
-        </div>
-      </div>
+     
       <div className="overflow-hidden relative flex-col justify-center items-center px-16 pt-64 pb-56 w-full text-5xl text-center text-white min-h-[698px] max-md:px-5 max-md:py-10 max-md:max-w-full max-md:text-4xl">
         <img
           loading="lazy"
@@ -43,6 +26,7 @@ function Home() {
         GrizzHacks Project
       </div>
     </div>
+    </>
   );
 }
 
