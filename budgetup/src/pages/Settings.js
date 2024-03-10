18b195333a72886f9e57a1b29cd55e8d1acc8972
William Gan <willg899@gmail.com>
2024-03-09 21:13:16 -0500

    put the icons in 1 button

--- budgetup/src/pages/Settings.js
+++ budgetup/src/pages/Settings.js
@@ -25,11 +25,23 @@ function Settings() {
     };
 
     return (
-        <div>
+        <>
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-            <button className="material-symbols-outlined" onClick={handleAccountRemoval}>person_remove</button>
-            <button className="material-symbols-outlined" onClick={handlePasswordReset}>lock_reset</button>
-        </div>
+            <div className="flex flex-col items-start pb-20 text-4xl font-bold text-black whitespace-nowrap bg-white">
+                <div className="justify-center items-start self-stretch px-10 py-6 w-full text-white bg-teal-300 max-md:px-5 max-md:max-w-full">
+                    Settings
+                </div>
+                <button className="flex gap-0 items-start px-5 mt-10 rounded-3xl bg-zinc-300 max-md:flex-wrap" onClick={handleAccountRemoval}>
+                    <span className="material-symbols-outlined pt-2">person_remove</span>
+                    <div className="flex-auto my-auto">Remove Account</div>
+                </button>
+                <button className="flex gap-1.5 px-5 py-0.5 mt-7 rounded-3xl bg-zinc-300 max-md:flex-wrap" onClick={handlePasswordReset}>
+                    <span className="material-symbols-outlined pt-2">lock_reset</span>
+                    <div className="flex-auto my-auto">Password Change</div>
+                </button>
+            </div>
+            
+        </>
     );
 }
 
