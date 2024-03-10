42ded547c27130d692738f46d2542adb1875941f
Schaubt <tristan.schaub@gmail.com>
2024-03-10 12:23:45 -0400

    add bar graph component

---
+++ budgetup/src/components/BarChart.js
@@ -0,0 +1,50 @@
+import { Bar } from "react-chartjs-2";
+
+const BarChart = ({ chartData }) => {
+  return (
+    <div className="chart-container">
+      <h2 style={{ textAlign: "center" }}>Bar Chart</h2>
+      <Bar
+        data={{
+          labels: [
+            "Groceries",
+            "Childcare",
+            "Rent/Mortgage",
+            "Restaurants",
+            "Debt Owed"
+          ],
+          datasets: [
+            {
+              label: "Last month",
+              data: chartData.dataset1,
+              backgroundColor: "rgba(75,192,192,1)",
+              borderColor: "rgba(75,192,192,1)",
+              borderWidth: 1
+            },
+            {
+              label: "This month",
+              data: chartData.dataset2,
+              backgroundColor: "rgba(255,99,132,1)",
+              borderColor: "rgba(255,99,132,1)",
+              borderWidth: 1
+            }
+          ]
+        }}
+        options={{
+          plugins: {
+            title: {
+              display: true,
+              text: "Compare monthly spending"
+            },
+            legend: {
+              display: true,
+              position: "top"
+            }
+          }
+        }}
+      />
+    </div>
+  );
+};
+
+export default BarChart;
